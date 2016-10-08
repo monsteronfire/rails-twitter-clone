d7d70f8808b27d087b79fa7eda462b7b30bec45c
monsteronfire <monsteronfire@gmail.com>
2016-10-08 21:19:46 +0800

    Add length validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,6 +19,16 @@ class UserTest < ActiveSupport::TestCase
 
   test "email should be present" do
     @user.email = " "
-    assert_not @email.valid?
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
   end
 end
