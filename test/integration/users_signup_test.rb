33533fa6d6ce18c64cfa9ca82b2eaade65b9f032
monsteronfire <monsteronfire@gmail.com>
2016-10-18 12:14:43 +0800

    Add error messages and accompanying tests

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'form[action="/signup"]'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'ul'
+    assert_select 'li', 'Name can\'t be blank'
+    assert_select 'li', 'Email is invalid'
+    assert_select 'li', 'Password confirmation doesn\'t match Password'
+    assert_select 'li', 'Password is too short (minimum is 6 characters)'
+  end
+
+  test "empty signup form" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name: "", email: "", password: "", password_confirmation: "" } }
+    end
+    assert_template 'users/new'
+    assert_select 'form[action="/signup"]'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'ul'
+    assert_select 'li', 'Name can\'t be blank'
+    assert_select 'li', 'Email can\'t be blank'
+    assert_select 'li', 'Email is invalid'
+    assert_select 'li', 'Password can\'t be blank'
+    assert_select 'li', 'Password is too short (minimum is 6 characters)'
+  end
+end
