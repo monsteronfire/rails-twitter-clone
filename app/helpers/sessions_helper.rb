403cb8544e3163b91d7ce7c8afe57ec4f26bedc6
monsteronfire <monsteronfire@gmail.com>
2016-10-25 11:41:30 +0800

    Add logged_in? method
    * Also add conditional dropdown menu for Account

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,4 +7,8 @@ module SessionsHelper
     # Below is the proper way to write: @current_user = @current_user || User.find_by(id: session[:user_id])
     @current_user ||= User.find_by(id: session[:user_id])
   end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
