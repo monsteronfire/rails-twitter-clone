96364b12a58ea214dc29cf138b04395c9a77de60
monsteronfire <monsteronfire@gmail.com>
2016-11-24 08:53:28 +0800

    Finish unsuccessful edit test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -9,6 +9,8 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
-
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: "", email: "foo@invalid", password: "foo", password_confirmation: "bar" } }
   end
 end
