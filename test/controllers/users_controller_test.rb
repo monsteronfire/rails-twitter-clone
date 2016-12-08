b4e3ef47f371d00ce569a4fcb3b44d86faf1d4d1
monsteronfire <monsteronfire@gmail.com>
2016-12-08 22:57:18 +0800

    Add admin users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,6 +11,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: { user: { password: 'foobar', password_confirmation: 'foobar', admin: true } }
+    assert_not @other_user.admin?
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
