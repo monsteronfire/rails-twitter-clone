a53b4f4a274f07a27ac4458449ba0ed2a795ff37
monsteronfire <monsteronfire@gmail.com>
2016-12-13 22:48:03 +0800

    Add activation test
    * Also refactor

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,6 +4,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -39,15 +43,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     #assert_select 'li', 'Password is too short (minimum is 6 characters)'
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: "Example User", email: "user@example.com", password:
      "foobar", password_confirmation: "foobar" } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    log_in_as(user)
+    assert_not is_logged_in?
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
-    #assert_template 'users/show'
-    #assert is_logged_in?
+    assert_template 'users/show'
+    assert is_logged_in?
     #assert_not flash.empty?
   end
 end
