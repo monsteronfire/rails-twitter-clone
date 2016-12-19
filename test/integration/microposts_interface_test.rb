a0fcd92f10bf8722a9ac7c070a984ad56ee488a6
monsteronfire <monsteronfire@gmail.com>
2016-12-19 21:54:39 +0800

    Add micropost picture test

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -12,14 +12,17 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type="file"]'
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
     end
     assert_select 'div#error_explanation'
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content, picture: picture } }
     end
+    assert micropost.picture?
     assert_redirected_to root_url
     follow redirect!
     assert_match content, response.body
